cf3ff057a292b3f5e27d471df009c8b1956da2ac
Benoit Arsenault <benoit.arsenault@gmail.com>
2019-01-15 01:55:43 -0500

    Cleanup

--- src/app/auth/actions/login.actions.ts
+++ src/app/auth/actions/login.actions.ts
@@ -1,25 +1,28 @@
-import { Action } from '@ngrx/store';
+import {Action} from '@ngrx/store';
 import {User} from '../models/user';
 
 export enum LoginActionTypes {
-  Login = '[Auth] Login User',
-  Logout = '[Auth] Logout User',
-  LoginSuccess = '[Auth] Login Success'
+    Login = '[Auth] Login User',
+    Logout = '[Auth] Logout User',
+    LoginSuccess = '[Auth] Login Success'
 }
 
 export class Login implements Action {
-  readonly type = LoginActionTypes.Login;
+    readonly type = LoginActionTypes.Login;
 
-  constructor(readonly payload: {username: string, password: string}) {}
+    constructor(readonly payload: { username: string, password: string }) {
+    }
 }
 
 export class LoginSuccess implements Action {
-  readonly type = LoginActionTypes.LoginSuccess;
-  constructor(readonly payload: User) {}
+    readonly type = LoginActionTypes.LoginSuccess;
+
+    constructor(readonly payload: User) {
+    }
 }
 
 export class Logout implements Action {
-  readonly type = LoginActionTypes.Logout;
+    readonly type = LoginActionTypes.Logout;
 }
 
 export type LoginActions = Login | Logout | LoginSuccess;
--- src/app/auth/effects/login.effects.ts
+++ src/app/auth/effects/login.effects.ts
@@ -1,35 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Login, LoginActionTypes, Logout} from '../actions/login.actions';
-import { Observable } from 'rxjs';
-import { Action } from '@ngrx/store';
-import {debounceTime, map, mergeMap, switchMap, tap, throttle, throttleTime} from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {Actions, Effect, ofType} from '@ngrx/effects';
+import {Login, LoginActionTypes, Logout} from '../actions/login.actions';
+import {Observable} from 'rxjs';
+import {Action} from '@ngrx/store';
+import {map, mergeMap, tap, throttleTime} from 'rxjs/operators';
 import {AuthService} from '../auth.service';
 import {MatSnackBar} from '@angular/material';
-import {subscribeTo, subscribeToObservable} from 'rxjs/internal-compatibility';
+
 
 // TODO: Fix snackbar hardcoded values using snackbar global settings
 // TODO: Fix throttleTime hardcoded values
 @Injectable()
 export class LoginEffects {
 
-  @Effect()
-  login$: Observable<Action> = this.actions$.pipe(
-      ofType<Login>(LoginActionTypes.Login),
-      throttleTime(1500),
-      mergeMap( action =>
-          this.authService.authenticate(action.payload.username, action.payload.password).pipe(
-            map(data => ({type: LoginActionTypes.LoginSuccess, payload: data}) )
-          )),
-      tap(action => this.snackbar.open(action.type, 'Dismiss', {duration: 3000}))
-  );
+    @Effect()
+    login$: Observable<Action> = this.actions$.pipe(
+        ofType<Login>(LoginActionTypes.Login),
+        throttleTime(1500),
+        mergeMap(action =>
+            this.authService.authenticate(action.payload.username, action.payload.password).pipe(
+                map(data => ({type: LoginActionTypes.LoginSuccess, payload: data}))
+            )),
+        tap(action => this.snackbar.open(action.type, 'Dismiss', {duration: 3000}))
+    );
 
-  @Effect({dispatch: false})
-  logout$: Observable<Action> = this.actions$.pipe(
-      ofType<Logout>(LoginActionTypes.Logout),
-      throttleTime(1500),
-      tap(action => this.snackbar.open(action.type, 'Dismiss', {duration: 3000}))
-  );
+    @Effect({dispatch: false})
+    logout$: Observable<Action> = this.actions$.pipe(
+        ofType<Logout>(LoginActionTypes.Logout),
+        throttleTime(1500),
+        tap(action => this.snackbar.open(action.type, 'Dismiss', {duration: 3000}))
+    );
 
-  constructor(private authService: AuthService, private snackbar: MatSnackBar, private actions$: Actions) {}
+    constructor(private authService: AuthService, private snackbar: MatSnackBar, private actions$: Actions) {
+    }
 }
